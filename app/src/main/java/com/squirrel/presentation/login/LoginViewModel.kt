6e9092969d8215d6ad17c67b40be8240781864f8
Gabriel KACI <gabriel.kaci75@gmail.com>
2020-02-28 18:54:52 +0100

    Fix catching login exception

--- app/src/main/java/com/squirrel/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/squirrel/presentation/login/LoginViewModel.kt
@@ -14,48 +14,57 @@ import javax.inject.Inject
 
 class LoginViewModel : ViewModel() {
 
-	@Inject
-	lateinit var keysUseCases: KeysUseCases
-	@Inject
-	lateinit var passwordUseCases: PasswordUseCases
-	@Inject
-	lateinit var lastFileUseCases: LastFileUseCases
+    @Inject
+    lateinit var keysUseCases: KeysUseCases
+    @Inject
+    lateinit var passwordUseCases: PasswordUseCases
+    @Inject
+    lateinit var lastFileUseCases: LastFileUseCases
 
-	var newFile = false
-	var selectedPath = MutableLiveData<String>("")
+    var newFile = false
+    var selectedPath = MutableLiveData<String>("")
 
-	init {
-		Squirrel.dagger.inject(this)
-		loadLastUsedPath()
-	}
+    init {
+        Squirrel.dagger.inject(this)
+        loadLastUsedPath()
+    }
 
-	fun hasRegisteredPassword(path: String, block: (Boolean) -> Unit) {
-		GlobalScope.launch(context = Dispatchers.Main) {
-			val password = passwordUseCases.checkPasswordExists(path)
-			block(password)
-		}
-	}
+    fun hasRegisteredPassword(path: String, block: (Boolean) -> Unit) {
+        GlobalScope.launch(context = Dispatchers.Main) {
+            val password = passwordUseCases.checkPasswordExists(path)
+            block(password)
+        }
+    }
 
-	fun getRegisteredPassword(key: String, block: (String) -> Unit) {
-		GlobalScope.launch {
-			block(passwordUseCases.getPassword(key))
-		}
-	}
+    fun getRegisteredPassword(key: String, block: (String) -> Unit) {
+        GlobalScope.launch {
+            block(passwordUseCases.getPassword(key))
+        }
+    }
 
-	private fun loadLastUsedPath() {
-		GlobalScope.launch {
-			selectedPath.postValue(lastFileUseCases.getLastFilePath())
-		}
-	}
+    private fun loadLastUsedPath() {
+        GlobalScope.launch {
+            selectedPath.postValue(lastFileUseCases.getLastFilePath())
+        }
+    }
 
-	fun loadData(path: String?, password: String?, block: (MutableList<Directory>) -> Unit) {
-		if (path != null && password != null) {
-			GlobalScope.launch {
-				keysUseCases.getKeys(path, password)?.toMutableList()?.let {
-					lastFileUseCases.saveLastFilePath(path)
-					block(it)
-				}
-			}
-		}
-	}
+    fun loadData(
+        path: String?,
+        password: String?,
+        success: (MutableList<Directory>) -> Unit,
+        fail: () -> Unit
+    ) {
+        if (path != null && password != null) {
+            GlobalScope.launch {
+                try {
+                    keysUseCases.getKeys(path, password)?.toMutableList()?.let {
+                        lastFileUseCases.saveLastFilePath(path)
+                        success(it)
+                    }
+                } catch (e: Exception) {
+                    fail()
+                }
+            }
+        }
+    }
 }
